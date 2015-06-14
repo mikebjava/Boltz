3026f41ec4a6f15538507ba31ccc75dddfbfbafa
mikebjava <Michael@10.0.0.15>
2015-06-14 16:48:13 -0400

    Tweaks to ol' Boltz
    More jump, more consistency between jumps, and a editor-tweakable jump
    cooldown timer.

--- Assets/scripts/player/controllers/JumpHandler.cs
+++ Assets/scripts/player/controllers/JumpHandler.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class JumpHandler : MonoBehaviour
@@ -8,6 +9,8 @@ public class JumpHandler : MonoBehaviour
     #region Component Variables
     public KeyCode jumpKey = KeyCode.Space;
     public float jumpHeight = 10f;
+    public float jumpCooldown = 1000;
+    public bool useJumpCooldown = true;
     public bool isGrounded = false;
     public GameObject groundCheck;
     public float offsetX = 0;
@@ -17,10 +20,14 @@ public class JumpHandler : MonoBehaviour
 
     private Rigidbody2D rb2d;
     private float groundCheckRadius = 0.5f;
+    private float lastJump = 0;
+    private Stopwatch jumpTimer;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        jumpTimer = new Stopwatch();
+        jumpTimer.Start();
     }
 
     void FixedUpdate()
@@ -29,7 +36,19 @@ public class JumpHandler : MonoBehaviour
 
         if (Input.GetKey(jumpKey) && isGrounded)
         {
-            Jump();
+            if (useJumpCooldown)
+            {
+                if (!IsOnCooldown())
+                {
+                    lastJump = jumpTimer.ElapsedMilliseconds;
+                    Jump();
+                }
+            }
+            else
+            {
+                lastJump = jumpTimer.ElapsedMilliseconds;
+                Jump();
+            }
         }
     }
 
@@ -40,5 +59,10 @@ public class JumpHandler : MonoBehaviour
             rb2d.AddForce(new Vector2(0, jumpHeight));
             isGrounded = false;
         }
+    }
+
+    private bool IsOnCooldown()
+    {
+        return ((jumpTimer.ElapsedMilliseconds - lastJump) <= jumpCooldown);
     }
 }
