1722cf4a262b3210c79ce49d8aa60f3f8352e85e
mikebjava <Michael@10.0.0.15>
2015-06-15 15:05:15 -0400

    Player Damage Events
    Structured a basic damage event system. Still need to work on Player
    Damage animations.

--- Assets/scripts/events/DamagedEventArgs.cs
+++ Assets/scripts/events/DamagedEventArgs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using UnityEngine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +8,8 @@ namespace Assets.scripts.events
 {
     public class DamagedEventArgs : EventArgs
     {
-
-
-
+        public double Damage { get; set; }
+        public GameObject DamagedObject { get; set; }
+        public GameObject DamagingObject { get; set; }
     }
 }
---
+++ Assets/scripts/player/PlayerVisualController.cs
@@ -0,0 +1,27 @@
+﻿using Assets.scripts.events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.scripts.player
+{
+    public class PlayerVisualController : MonoBehaviour
+    {
+
+        private PlayerVitalsController vitals;
+
+        void Start()
+        {
+            vitals = gameObject.GetComponent<PlayerVitalsController>() as PlayerVitalsController;
+            vitals.Damaged += OnPlayerDamaged;
+        }
+
+        private void OnPlayerDamaged(object sender, DamagedEventArgs e)
+        {
+            // TODO: Add player visual effects for being damaged
+        }
+
+    }
+}
