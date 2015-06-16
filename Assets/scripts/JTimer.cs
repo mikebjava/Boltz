3b333ae30a99af05a538fb9c1996762300c193da
CodeWalker1337 <jrgolden2011@live.com>
2015-06-16 00:09:34 -0400

    Dev Changelog J-0.0.4
    added fonts
    broken code for time(because unity 5 is "good")

---
+++ Assets/scripts/JTimer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JTimer : MonoBehaviour {
+    public float timeLeft;
+    public GameObject go;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time Left " + timeLeft;
+    }
+}
