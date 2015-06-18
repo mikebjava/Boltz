d1478fad5c737d6a9c341f346c228bb48f13a9bd
mikebjava <Michael@10.0.0.15>
2015-06-18 14:55:11 -0400

    Dev Changelog M-0.0.4
    - Added sounds for picking up Items and Collision with Boltz's feet
    - Gave Boltz an AudioSource

--- Assets/scripts/collectables/Collectable.cs
+++ Assets/scripts/collectables/Collectable.cs
@@ -7,6 +7,11 @@ public class Collectable : MonoBehaviour
 
     #region Editor Variables
     public bool DestroyOnCollect = true;
+    public bool PlaySoundOnCollect = true;
+    public AudioClip CollectionSound;
+    public bool UsePitchVariation = true;
+    public float PitchLowRange = 0.7f;
+    public float PitchHighRange = 1.0f;
     #endregion
 
     #region Events
@@ -24,11 +29,29 @@ public class Collectable : MonoBehaviour
     {
         if (coll.gameObject.tag == "Player")
         {
-            if (DestroyOnCollect)
-                Destroy(gameObject);
             CollectableCollectedEventArgs args = new CollectableCollectedEventArgs();
             args.Collector = coll.gameObject;
             OnCollected(this, args);
+
+            AudioSource source = GameController.Instance().Boltz.GetComponent<AudioSource>() as AudioSource;
+            if (source == null && PlaySoundOnCollect)
+            {
+                Debug.LogWarning("AudioSource not found while trying to play a Collectable's OnCollect sound.");
+            }
+
+            if (source != null && PlaySoundOnCollect && CollectionSound != null)
+            {
+                if (UsePitchVariation)
+                {
+                    source.pitch = UnityEngine.Random.Range(PitchLowRange, PitchHighRange);
+                }
+
+                source.PlayOneShot(CollectionSound, 1.0f);
+            }
+
+            if (DestroyOnCollect)
+                Destroy(this.gameObject);
+
         }
     }
 }
---
+++ Assets/scripts/player/FeetAudioController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeetAudioController : MonoBehaviour
+{
+    #region Editor Variables
+    public AudioClip JumpLandSound;
+    #endregion
+
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GameController.Instance().Boltz.GetComponent<AudioSource>();
+        if (source == null)
+        {
+            Debug.LogWarning("FeetAudioController was unable to find the player's AudioSource.");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (source != null)
+        {
+            source.pitch = 1.0f;
+            source.PlayOneShot(JumpLandSound, 1.0f);
+        }
+    }
+}
