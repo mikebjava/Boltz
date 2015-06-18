53f5e6c2dc1799b4c6c5dce3945ed1c4e034ef24
CodeWalker1337 <jrgolden2011@live.com>
2015-06-17 23:54:12 -0400

    Dev Changelog J-0.0.8
    -------------J-0.0.8----------------------
    ~~NEW~~
    -Added Timer
    -Added Experimental collision event
    -Added first power up called TimePowerUp this will add time to the timer
    (duh)
    ~~TODO~~
    -Add image for time power up
    -Animate time power up image
    -Add better support for power ups

--- Assets/scripts/JTimer.cs
+++ Assets/scripts/JTimer.cs
@@ -1,19 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public class JTimer : MonoBehaviour {
     public float timeLeft;
-    public GameObject go;
+    public Text time;
+    public bool startOnPlay;
+    private bool started;
     //public Text text;
-	// Use this for initialization
-	void Start () {
-        
+
+    #region Events
+    public event EventHandler<TimerEventArgs> timeChanged;
+    public event EventHandler<TimerEventArgs> timerStarted;
+    public event EventHandler<TimerEventArgs> timerStopped;
+    public virtual void onTimeChanged(object obj, TimerEventArgs args)
+    {
+        if(timeChanged != null)
+        {
+            timeChanged(obj, args);
+        }
+    }
+
+    public virtual void onTimerStarted(object obj, TimerEventArgs args)
+    {
+        if(timerStarted != null)
+        {
+            timerStarted(obj, args);
+        }
+    }
+    public virtual void onTimerStopped(object obj, TimerEventArgs args)
+    {
+        if (timerStopped != null)
+        {
+            timerStarted(obj, args);
+        }
+    }
+    #endregion
+
+    // Use this for initialization
+	void Start () 
+    {
+        if(time == null)
+        {
+            Debug.LogWarning("Text component for timer was not found. This may cause errors.");
+        } else if (startOnPlay)
+        {
+            startTimer(timeLeft);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        timeLeft -= Time.deltaTime;
-        //text.text = "Time Left " + timeLeft;
+        if (started)
+        {
+            timeLeft -= Time.deltaTime;
+            time.text = "Time Left: " + (int)timeLeft;
+            timeChange();
+        }
     }
+
+    #region Timer Methods
+    public void startTimer(float time)
+    {
+        timeLeft = time;
+        TimerEventArgs args = new TimerEventArgs();
+        args.Time = time;
+        started = true;
+        onTimerStarted(this, args);
+    }
+    public void stopTimer()
+    {
+        TimerEventArgs args = new TimerEventArgs();
+        args.Time = timeLeft;
+        started = false;
+        onTimerStopped(this, args);
+    }
+    public void addTime(float time)
+    {
+        timeLeft += time;
+        timeChange();
+    }
+    public void removeTime(float time)
+    {
+        timeLeft -= time;
+        timeChange();
+    }
+    private void timeChange()
+    {
+        TimerEventArgs args = new TimerEventArgs();
+        args.Time = timeLeft;
+        onTimeChanged(this, args);
+    }
+    #endregion
 }
---
+++ Assets/scripts/events/CollisionEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CollisionEventArgs : EventArgs 
+{
+    public Collider2D triggerObject { get; set; }
+
+    public Collision2D collisionObject { get; set; }
+
+    public bool isTrigger { get; set; }
+}
