72b6e0732d8538acd931e5b13bf56790a5064de8
mikebjava <Michael@10.0.0.15>
2015-06-18 21:52:34 -0400

    Fun with Physics
    - Add some experimental game mechanics

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    public bool explode = false;
+
+    void FixedUpdate()
+    {
+        if (explode)
+        {
+            PointEffector2D p = gameObject.GetComponent<PointEffector2D>() as PointEffector2D;
+            if (p != null)
+            {
+                p.enabled = true;
+
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (explode)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
