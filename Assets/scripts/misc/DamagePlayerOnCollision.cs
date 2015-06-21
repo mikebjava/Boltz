904e287ca1e8673c5c22a676792dd7ca18baff1e
CodeWalker1337 <jrgolden2011@live.com>
2015-06-21 01:08:01 -0400

    Damage Player Script Added
    Get Shrekted

---
+++ Assets/scripts/misc/DamagePlayerOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamagePlayerOnCollision : MonoBehaviour {
+    public int damage;
+    PlayerVitalsController pvc;
+	// Use this for initialization
+	void Start () {
+        pvc = GameController.Instance().Boltz.GetComponent<PlayerVitalsController>() as PlayerVitalsController;
+        if(pvc == null)
+        {
+            Debug.LogWarning("PVC NOT FOUND ITS A TRAP");
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Player")
+        {
+            pvc.Damage(damage, this.gameObject);
+        }
+    }
+
+
+}
