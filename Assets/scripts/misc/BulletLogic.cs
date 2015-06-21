50f8cf77055d2130d9e3e0d432174110d1ed7b71
mikebjava <Michael@10.0.0.15>
2015-06-21 07:27:53 -0400

    Formatting Code
    - Teh codez

--- Assets/scripts/misc/BulletLogic.cs
+++ Assets/scripts/misc/BulletLogic.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BulletLogic : MonoBehaviour {
+public class BulletLogic : MonoBehaviour
+{
     public float speed;
     private Rigidbody2D rigidbody;
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
-        if(rigidbody == null)
+        if (rigidbody == null)
         {
             Debug.LogWarning("Rigidbody for bullet not found");
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector2 spd = new Vector2(speed, 0);
         rigidbody.AddForce(spd);
-	}
+    }
 }
--- Assets/scripts/misc/Turret.cs
+++ Assets/scripts/misc/Turret.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Turret : MonoBehaviour {
+public class Turret : MonoBehaviour
+{
     public GameObject bullet;
-  
-	// Use this for initialization
-	void Start () {
-    
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
-       shoot();
-	}
 
-    BulletLogic shoot()
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+    }
+
+    private BulletLogic shoot()
     {
         GameObject newObject = Instantiate(bullet) as GameObject;
         BulletLogic bl = newObject.GetComponent<BulletLogic>() as BulletLogic;
