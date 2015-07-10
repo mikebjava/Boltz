700a4677fc36a6d09418f8c05ba3ce2b9d22f63c
CodeWalker1337 <jrgolden2011@live.com>
2015-07-10 00:27:42 -0400

    Save Game
    Save Game Method created no load game yet.
    Saves game in .slf(save level file) file format.

---
+++ Assets/scripts/misc/GameSaveController.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameSaveController : MonoBehaviour
+{
+    private int levelID;
+    #region Events
+    public event EventHandler<SaveGameEventArgs> Saved;
+    public virtual void onSaved(object obj, SaveGameEventArgs args)
+    {
+        if(Saved != null)
+        {
+            Saved(obj, args);
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        levelID = Application.loadedLevel;
+    }
+
+    public void CreateSaveFile()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream fs = File.Create(Application.persistentDataPath + "/saveGame.slf");
+        bf.Serialize(fs, levelID);
+        fs.Close();
+        SaveGameEventArgs args = new SaveGameEventArgs();
+        args.Player = null;
+        args.LevelName = null;
+        args.SaveLevel = levelID;
+        onSaved(this, args);
+    }
+}
---
+++ Assets/scripts/player/events/SaveGameEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class SaveGameEventArgs : EventArgs
+{
+    public GameObject Player { get; set; }
+    public int SaveLevel { get; set; }
+    public string LevelName { get; set; }
+}
